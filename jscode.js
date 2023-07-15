99be0891ce20dec246e3c5233fe37f3f63c6c764
sharib-akhtar <139340514+sharib-akhtar@users.noreply.github.com>
2023-07-16 00:29:10 +0530

    Update jscode.js

--- jscode.js
+++ jscode.js
@@ -69,7 +69,7 @@ function testOfStrength()
                 }
             }
              
-           if((k==0 && m==0) ||(k==0 || m==0)) 
+           if(k==0 && m==0) 
                 {
                     const newElement=document.createElement('p');
                     const newText=document.createTextNode("Your Password is  Weak!!!\n It should be a combination of numbers and special characters");
@@ -77,9 +77,6 @@ function testOfStrength()
                     newElement.appendChild(newText);
 
                 }
-                
-            
-                
             else{
                 const newElement=document.createElement('p');
                 const newText=document.createTextNode("Your Password holds good strength...Congrats");
